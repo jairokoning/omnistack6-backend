d418bc57ab26b6adc7968a813247f738948d338b
jairokoning <jairo.koning@gmail.com>
2019-06-15 16:52:25 -0300

    Projeto NodeJS

---
+++ README.md
@@ -0,0 +1,2 @@
+# omnistack6-backend
+Projeto desenvolvido na semana OmniStack6.0 da RocketSeat
---
+++ src/controllers/BoxController.js
@@ -0,0 +1,29 @@
+const Box =  require('../models/Box')
+const mongoose = require('mongoose')
+
+class BoxController{
+
+    async store(req, res){
+
+        const box = await Box.create(req.body)
+
+        return res.json(box)
+    }
+
+    async show(req, res){
+        try {
+
+            const box = await Box.findById(req.params.id).populate({
+                path: 'files',
+                options:  {sort: {createdAt: -1}}
+            })
+            return res.json(box)
+                    
+        } catch (error) {
+           console.log(error) 
+        }
+    }
+
+}
+
+module.exports = new BoxController()
